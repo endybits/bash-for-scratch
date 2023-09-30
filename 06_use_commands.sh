35ee6c557b98f68201491fa9a05b584f02a57b08
Endy B <endyb.dev@gmail.com>
2023-09-30 15:37:33 +1000

    use linux command in our code

---
+++ 06_use_commands.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+: '{
+    "Dev": "Endy B.",
+    "github": "https://github.com/endybits"
+    "linkedin": "https://www.linkedin.com/in/endyb/"
+    }'
+
+# Code for execute Linux commands in our bash code.
+location=`pwd`
+file_list=$(ls -al)
+echo -e "The location of this file is:\n $location"
+echo -e "The file list in this directory is:\n $file_list"
