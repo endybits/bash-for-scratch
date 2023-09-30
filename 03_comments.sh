92b37fdb3b9cc021b001a2f0f95e8aa6d0e8d46c
Endy B <endyb.dev@gmail.com>
2023-09-30 13:05:21 +1000

    Comments

---
+++ 03_comments.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# This is a comment for only one line
+echo "Comments are only visible into the code and do not in their execution outputs"
+
+: ' This is a 
+    multi-line comment
+    :)'
